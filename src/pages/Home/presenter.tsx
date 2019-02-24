ebec12d07900dcb6ce65cfd083fd92b8fe5f0c91
Andi Pätzold <andi.paetzold@gmail.com>
2019-02-24 17:15:52 +0100

    feat(home) add some stats

--- src/pages/Home/presenter.tsx
+++ src/pages/Home/presenter.tsx
@@ -1,13 +1,52 @@
 import AuthUserContext from "hoc/withAuthUser/context";
 import { useContext } from "react";
 import React from "react";
+import { Card, Statistic, Col, Row } from "antd";
+import useUser from "hooks/useUser";
+import useSubjects from "hooks/useSubjects";
 
 export default function Home() {
-  const user = useContext(AuthUserContext);
+  const user = useUser();
 
-  if (!user) {
-    return null;
-  }
+  const { loading, subjects } = useSubjects();
 
-  return <>Hello {user.displayName}</>;
+  return (
+    <Card title={`Hello ${user.displayName}`} loading={loading}>
+      <Row>
+        <Col span={6}>
+          {subjects && <Statistic title="Subjects" value={subjects.length} />}
+        </Col>
+        <Col span={6}>
+          {subjects && (
+            <Statistic
+              title="Cards to Learn"
+              value={subjects
+                .map(s => s.cardsCount - (s.cardsPhase[6] || 0))
+                .reduce((prev, cur) => prev + cur, 0)}
+            />
+          )}
+        </Col>
+        <Col span={6}>
+          {subjects && (
+            <Statistic
+              title="Cards in Phase 6"
+              value={subjects
+                .map(s => s.cardsPhase[6] || 0)
+                .reduce((prev, cur) => prev + cur, 0)}
+            />
+          )}
+        </Col>
+        <Col span={6}>
+          {subjects && (
+            <Statistic
+              title="Number of Cards"
+              value={subjects
+                .map(s => s.cardsCount)
+                .reduce((prev, cur) => prev + cur, 0)}
+            />
+          )}
+        </Col>
+      </Row>
+    </Card>
+  );
 }
