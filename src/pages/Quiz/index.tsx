07c023953711f397600b0bf681ce7b718f150563
Andi Pätzold <andi.paetzold@gmail.com>
2019-02-24 15:17:33 +0100

    feat(quiz) add subject overview

--- src/pages/Quiz/index.tsx
+++ src/pages/Quiz/index.tsx
@@ -1,7 +1,44 @@
+import { Button, Card, Icon, Table } from "antd";
+import useSubjects from "hooks/useSubjects";
 import React from "react";
+import { RouteComponentProps } from "react-router";
+import { getTodayCardCount } from "util/subject";
 
-export default class Quiz extends React.PureComponent {
-  render() {
-    return "Quiz";
-  }
+type Props = {} & RouteComponentProps;
+
+export default function Quiz({ history }: Props) {
+  const { loading, subjects } = useSubjects();
+
+  return (
+    <Card title="Quiz" loading={loading}>
+      <Table
+        dataSource={subjects}
+        columns={[
+          {
+            title: "Name",
+            dataIndex: "name",
+            key: "name"
+          },
+          {
+            title: "Today's cards",
+            key: "today",
+            render: getTodayCardCount
+          },
+          {
+            title: "Action",
+            key: "action",
+            render: ({ id }) => (
+              <Button
+                type="primary"
+                size="small"
+                onClick={() => history.push(`/quiz/${id}`)}
+              >
+                <Icon type="play-circle" /> Go
+              </Button>
+            )
+          }
+        ]}
+      />
+    </Card>
+  );
 }
