61397fc4fb7ebc0c3101f0b71b8a141dd68c38c3
Andi Pätzold <andi.paetzold@gmail.com>
2019-02-24 02:30:38 +0100

    feat(functions) setup functions

---
+++ src/functions/index.ts
@@ -0,0 +1,4 @@
+import { initializeApp } from "firebase-admin";
+import "source-map-support/register";
+
+initializeApp();
---
+++ webpack.functions.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const appConfig = {
+  cache: true,
+  devtool: "source-map",
+
+  entry: path.resolve(__dirname, "./src/functions/index.ts"),
+  output: {
+    filename: "./index.js",
+    path: path.resolve(__dirname, "./dist/functions"),
+    libraryTarget: "this"
+  },
+
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig.functions.json"
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  externals: [nodeExternals()],
+
+  plugins: [
+    new CopyWebpackPlugin([{ from: "package.json", to: "package.json" }])
+  ]
+};
+
+module.exports = appConfig;
