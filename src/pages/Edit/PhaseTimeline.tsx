0ebf5e2f587ec0642079bd1c3b07a411fad53685
Andi Pätzold <andi.paetzold@gmail.com>
2019-02-24 16:42:09 +0100

    fix(subject) add key

--- src/pages/Edit/PhaseTimeline.tsx
+++ src/pages/Edit/PhaseTimeline.tsx
@@ -10,7 +10,7 @@ export default function PhaseTimeline({ subject }: Props) {
   return (
     <Timeline style={{ marginBottom: "-50px" }}>
       {[1, 2, 3, 4, 5, 6].map(phase => (
-        <Timeline.Item>
+        <Timeline.Item key={phase}>
           Phase {phase}: {subject.cardsPhase[phase] || 0}
         </Timeline.Item>
       ))}
