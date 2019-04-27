572c1a72bf2298f1ff3dbf234db13cff87dddf90
Andi Pätzold <andi.paetzold@gmail.com>
2019-04-27 22:44:13 +0200

    feat(build) add terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,72 +2,87 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { GenerateSW } = require("workbox-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = (env, options) => ({
-    entry: {
-        app: "./src/index.tsx"
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        modules: [path.resolve("./node_modules"), path.resolve("./src")]
-    },
-    output: {
-        path: path.join(__dirname, "/dist/app"),
-        filename: options.mode === "production" ? "[name].[hash].js" : "[name].js",
-        chunkFilename: options.mode === "production" ? "[name].[hash].js" : "[name].js",
-        publicPath: "/"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    chunks: "all",
-                    test: /[\\/]node_modules[\\/]/,
-                    enforce: true,
-                    priority: 1
-                }
-            }
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        new CopyPlugin([{ from: "*.png", to: "assets", context: "src/assets" }, { from: "src/manifest.json", to: "manifest.json" }]),
-        new GenerateSW({
-            runtimeCaching: [
+module.exports = (env, options) => {
+    const dev = options.mode === "development";
+
+    return {
+        entry: {
+            app: "./src/index.tsx"
+        },
+        devServer: {
+            historyApiFallback: true
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+            modules: [path.resolve("./node_modules"), path.resolve("./src")]
+        },
+        output: {
+            path: path.join(__dirname, "/dist/app"),
+            filename: dev ? "[name].js" : "[name].[hash].js",
+            chunkFilename: dev ? "[name].js" : "[name].[hash].js",
+            publicPath: "/"
+        },
+
+        devtool: dev ? "source-map" : "none",
+
+        module: {
+            rules: [
                 {
-                    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-                    handler: "CacheFirst",
-                    options: {
-                        cacheName: "images"
-                    }
+                    test: /\.tsx?$/,
+                    loader: "ts-loader"
                 },
-                { urlPattern: /\.(?:js|html|json)$/, handler: "StaleWhileRevalidate" }
-            ],
-            navigateFallback: "/index.html"
-        })
-    ]
-});
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: "style-loader"
+                        },
+                        {
+                            loader: "css-loader"
+                        }
+                    ]
+                }
+            ]
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        chunks: "all",
+                        test: /[\\/]node_modules[\\/]/,
+                        enforce: true,
+                        priority: 1
+                    }
+                }
+            },
+            minimizer: [
+                new TerserPlugin({
+                    cache: dev,
+                    sourceMap: dev,
+                    parallel: true
+                })
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            new CopyPlugin([{ from: "*.png", to: "assets", context: "src/assets" }, { from: "src/manifest.json", to: "manifest.json" }]),
+            new GenerateSW({
+                runtimeCaching: [
+                    {
+                        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "images"
+                        }
+                    },
+                    { urlPattern: /\.(?:js|html|json)$/, handler: "StaleWhileRevalidate" }
+                ],
+                navigateFallback: "/index.html"
+            })
+        ]
+    };
+};
