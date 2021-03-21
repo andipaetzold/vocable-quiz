128af66473a3f7e8448b79924193e5a6f0f7fb48
Andi Pätzold <github@andipaetzold.com>
2021-03-21 09:05:15 +0100

    fix: use tsconfig.functions.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from "rollup-plugin-typescript2";
-import copy from 'rollup-plugin-copy'
+import copy from "rollup-plugin-copy";
 import pkg from "./package.json";
+import path from "path";
 
 export default {
     input: "src/functions/index.ts",
@@ -14,11 +15,10 @@ export default {
     plugins: [
         typescript({
             typescript: require("typescript"),
+            tsconfig: path.resolve(__dirname, "./tsconfig.functions.json"),
         }),
         copy({
-          targets: [
-            { src: 'src/functions/package.json', dest: 'build-functions' },
-          ]
-        })
+            targets: [{ src: "src/functions/package.json", dest: "build-functions" }],
+        }),
     ],
 };
